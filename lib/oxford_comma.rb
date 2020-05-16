d73b493f0c8bd8fe38c43d7ed48800e4ed89f030
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-16 22:31:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 
 
 def oxford_comma(array)
-  if array.length > 1 array =[array]
-  array.join()
-  if array.length == 2
-  array.join(" and ")
-end
+  if array.length > 1 
+   return array.join()
+  elsif array.length == 2
+    return array.join(" and ")
+  end
 end
